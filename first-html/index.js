2e5b74db70227b84e8037a99ba834dc39704a8b5
Pedro Henrique <edysegura@gmail.com>
2017-06-03 14:29:33 -0300

    Comitando

--- first-html/index.js
+++ first-html/index.js
@@ -1,8 +1,11 @@
 console.log("Hi there!");
 
-function sleep(miliseconds){
-    let startTime = Date.now();
-    while(Date.now() < startTime + miliseconds);
+/*"""function sleep(miliseconds){
+"""    let startTime = Date.now();
+//"""    while(Date.now() < startTime + miliseconds);
+"""}*/
+for(let i=0; i<10;i++){
+    console.log('Hi number '++i)
 }
 sleep(2000);
 
